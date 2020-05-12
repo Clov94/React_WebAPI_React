443b41ed8d6cbb376df118a124e2056a635a65fb
Clov94 <Clov94@users.noreply.github.com>
2020-05-12 11:30:13 -0500

    React_WebAPI | department component rendered properly

--- src/components/department-component.js
+++ src/components/department-component.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 
 export class DepartmentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { departments: [] };
+  }
+
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList() {
+    this.setState({
+      departments: [
+        { DeparmentID: 1, DepartmentName: "IT" },
+        { DeparmentID: 2, DepartmentName: "Support" },
+      ],
+    });
+  }
+
   render() {
+    const { departments } = this.state;
     return (
-      <div className="mt-5 d-flex justify-content-left">
-        <h3>This is the Department page</h3>
-      </div>
+      <Table className="mt-4" striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>DepartmentID</th>
+            <th>DepartmentName</th>
+          </tr>
+        </thead>
+        <tbody>
+          {departments.map(({ DeparmentID, DepartmentName }) => (
+            <tr key={DeparmentID}>
+              <td>{DeparmentID}</td>
+              <td>{DepartmentName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     );
   }
 }
